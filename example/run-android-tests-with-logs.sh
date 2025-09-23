e9e2ac15e6f8dba97f7c7b0a6e9a5f922e39bd1e
jlake <jim@blueskylabs.com>
2025-09-23 12:58:09 -0700

    cleanup

--- example/run-android-tests-with-logs.sh
+++ example/run-android-tests-with-logs.sh
@@ -30,7 +30,7 @@ cd ../android
 adb logcat -c
 
 # Start logcat in background to capture test logs
-adb logcat -v time | grep --line-buffered -E "(System.out|TestRunner|sshsftpexample|SSHClient)" &
+adb logcat -v time | grep --line-buffered -E "(System.out|TestRunner|SSHClient)" &
 LOGCAT_PID=$!
 
 # Give logcat a moment to start

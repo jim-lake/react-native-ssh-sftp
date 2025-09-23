0ccc815ba5f8956c75dc128fa3b28e45e5046ff7
jlake <jim@blueskylabs.com>
2025-09-23 09:15:02 -0700

    cleaner test

--- example/android/app/src/androidTest/java/com/sshsftpexample/DetoxTest.java
+++ example/android/app/src/androidTest/java/com/sshsftpexample/DetoxTest.java
@@ -29,57 +29,6 @@ public class DetoxTest {
         device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
     }
 
-    // @Test
-    public void testAppLaunch() throws Exception {
-        Intent intent = new Intent();
-        mActivityRule.launchActivity(intent);
-        Thread.sleep(3000);
-        
-        UiObject title = device.findObject(new UiSelector().text("SSH SFTP Example"));
-        assert(title.exists());
-        
-        UiObject status = device.findObject(new UiSelector().textContains("Status:"));
-        assert(status.exists());
-        
-        UiObject testButton = device.findObject(new UiSelector().text("Test SSH"));
-        UiObject dockerButton = device.findObject(new UiSelector().text("Docker SSH"));
-        UiObject rsaButton = device.findObject(new UiSelector().text("RSA Key"));
-        
-        assert(testButton.exists());
-        assert(dockerButton.exists());
-        assert(rsaButton.exists());
-    }
-
-    // @Test
-    public void testBasicSSH() throws Exception {
-        Intent intent = new Intent();
-        mActivityRule.launchActivity(intent);
-        Thread.sleep(3000);
-        
-        UiObject button = device.findObject(new UiSelector().text("Test SSH"));
-        assert(button.exists());
-        button.click();
-        Thread.sleep(6000);
-        
-        UiObject anyButton = device.findObject(new UiSelector().className("android.widget.TextView").textContains("SSH"));
-        assert(anyButton.exists());
-    }
-
-    // @Test
-    public void testDockerSSH() throws Exception {
-        Intent intent = new Intent();
-        mActivityRule.launchActivity(intent);
-        Thread.sleep(3000);
-        
-        UiObject button = device.findObject(new UiSelector().text("Docker SSH"));
-        assert(button.exists());
-        button.click();
-        Thread.sleep(10000);
-        
-        UiObject anyButton = device.findObject(new UiSelector().className("android.widget.TextView").textContains("SSH"));
-        assert(anyButton.exists());
-    }
-
     @Test
     public void testRSAKey() throws Exception {
         Intent intent = new Intent();
@@ -89,12 +38,12 @@ public class DetoxTest {
         // Check if app launched
         UiObject title = device.findObject(new UiSelector().text("SSH SFTP Example"));
         System.out.println("Title exists: " + title.exists());
-        assert(title.exists());
+        assertTrue("Title should exist", title.exists());
         
         // Find RSA Key button
         UiObject rsaButton = device.findObject(new UiSelector().text("RSA Key"));
         System.out.println("RSA Key button exists: " + rsaButton.exists());
-        assert(rsaButton.exists());
+        assertTrue("rsa button should exist", rsaButton.exists());
         
         rsaButton.click();
         System.out.println("RSA Key button clicked, waiting for authentication...");
@@ -128,125 +77,5 @@ public class DetoxTest {
         }
 
         assertTrue("Authentication should succeed", successText.exists());
-    }
-
-    // @Test
-    public void testOpenSSHKey() throws Exception {
-        Intent intent = new Intent();
-        mActivityRule.launchActivity(intent);
-        Thread.sleep(3000);
-        
-        UiObject button = device.findObject(new UiSelector().text("OpenSSH Key"));
-        assert(button.exists());
-        button.click();
-        Thread.sleep(8000);
-        
-        UiObject anyButton = device.findObject(new UiSelector().className("android.widget.TextView").textContains("SSH"));
-        assert(anyButton.exists());
-    }
-
-    // @Test
-    public void testEncryptedRSA() throws Exception {
-        Intent intent = new Intent();
-        mActivityRule.launchActivity(intent);
-        Thread.sleep(3000);
-        
-        UiObject button = device.findObject(new UiSelector().text("Encrypted RSA"));
-        assert(button.exists());
-        button.click();
-        Thread.sleep(8000);
-        
-        UiObject anyButton = device.findObject(new UiSelector().className("android.widget.TextView").textContains("SSH"));
-        assert(anyButton.exists());
-    }
-
-    // @Test
-    public void testSFTP() throws Exception {
-        Intent intent = new Intent();
-        mActivityRule.launchActivity(intent);
-        Thread.sleep(3000);
-        
-        UiObject button = device.findObject(new UiSelector().text("SFTP Test"));
-        assert(button.exists());
-        button.click();
-        Thread.sleep(10000);
-        
-        UiObject anyButton = device.findObject(new UiSelector().className("android.widget.TextView").textContains("SSH"));
-        assert(anyButton.exists());
-    }
-
-    // @Test
-    public void testRSA2048Sign() throws Exception {
-        Intent intent = new Intent();
-        mActivityRule.launchActivity(intent);
-        Thread.sleep(3000);
-        
-        UiObject button = device.findObject(new UiSelector().text("RSA 2048 Sign"));
-        assert(button.exists());
-        button.click();
-        Thread.sleep(8000);
-        
-        UiObject anyButton = device.findObject(new UiSelector().className("android.widget.TextView").textContains("SSH"));
-        assert(anyButton.exists());
-    }
-
-    // @Test
-    public void testRSA2048SHA256Sign() throws Exception {
-        Intent intent = new Intent();
-        mActivityRule.launchActivity(intent);
-        Thread.sleep(3000);
-        
-        UiObject button = device.findObject(new UiSelector().text("RSA 2048 SHA256 Sign"));
-        assert(button.exists());
-        button.click();
-        Thread.sleep(8000);
-        
-        UiObject anyButton = device.findObject(new UiSelector().className("android.widget.TextView").textContains("SSH"));
-        assert(anyButton.exists());
-    }
-
-    // @Test
-    public void testRSA4096Sign() throws Exception {
-        Intent intent = new Intent();
-        mActivityRule.launchActivity(intent);
-        Thread.sleep(3000);
-        
-        UiObject button = device.findObject(new UiSelector().text("RSA 4096 Sign"));
-        assert(button.exists());
-        button.click();
-        Thread.sleep(8000);
-        
-        UiObject anyButton = device.findObject(new UiSelector().className("android.widget.TextView").textContains("SSH"));
-        assert(anyButton.exists());
-    }
-
-    // @Test
-    public void testECDSASign() throws Exception {
-        Intent intent = new Intent();
-        mActivityRule.launchActivity(intent);
-        Thread.sleep(3000);
-        
-        UiObject button = device.findObject(new UiSelector().text("ECDSA P-256 Sign"));
-        assert(button.exists());
-        button.click();
-        Thread.sleep(8000);
-        
-        UiObject anyButton = device.findObject(new UiSelector().className("android.widget.TextView").textContains("SSH"));
-        assert(anyButton.exists());
-    }
-
-    // @Test
-    public void testEd25519Sign() throws Exception {
-        Intent intent = new Intent();
-        mActivityRule.launchActivity(intent);
-        Thread.sleep(3000);
-        
-        UiObject button = device.findObject(new UiSelector().text("Ed25519 Sign"));
-        assert(button.exists());
-        button.click();
-        Thread.sleep(8000);
-        
-        UiObject anyButton = device.findObject(new UiSelector().className("android.widget.TextView").textContains("SSH"));
-        assert(anyButton.exists());
     }
 }
